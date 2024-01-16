20a8b0ba95e525c2e27688f4d87d312255af59c5
Nathan <nathopp@gmail.com>
2024-01-16 17:01:25 -0600

    Phase-2

--- Program.cs
+++ Program.cs
@@ -1,9 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+
+int secretNumber = 42;
+
+
 Console.WriteLine("Welcome to the Guessing Game!");
 Console.Write("Guess the secret number: ");
 
-string userGuess = Console.ReadLine();
+
+int userGuess = int.Parse(Console.ReadLine());
 
 
-Console.WriteLine("Your guess: " + userGuess);
+if (userGuess == secretNumber)
+{
+    Console.WriteLine("Congratulations! You guessed the correct number.");
+}
+else
+{
+    Console.WriteLine("Sorry, that's not correct. Better luck next time!");
+}
+
