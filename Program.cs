b744e32da7dbe8c472c914624a72cb57ebe628da
Nathan <nathopp@gmail.com>
2024-01-16 13:14:05 -0600

    Phase 1 complete

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Welcome to the Guessing Game!");
+Console.Write("Guess the secret number: ");
+
+string userGuess = Console.ReadLine();
+
+
+Console.WriteLine("Your guess: " + userGuess);
